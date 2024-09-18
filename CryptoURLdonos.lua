fdf9fc3fc9ca811873f4bdf58efcc127498f23f8
u/Failed_cocacola <123872886+localscripts@users.noreply.github.com>
2024-09-18 06:45:56 -0700

    Delete CryptoURLdonos.lua

--- CryptoURLdonos.lua
+++
@@ -1,5 +0,0 @@
-BTC address:                                              bc1q25l9ewkkewag0wz0srnjk9whr8u4hgyk2l8mk8
-ETH (ETH Network)', address:                              0xa3095ecC94C75364cb9A0E1EE967Fbcb1C9B24B5
-LTC (LTC Network)', address:                              LN619o1C1q5aArLRMnsX7myTMaHjAT3sQ8
-XMR', address:                                            49n8RRi9zuGPvXjw6V5fHcPXqU528gKzrCgSPmbbKYDe7sKM7ekSghrhypamV5cRyLhMsUtHKybsr2CfSEPnLzBrMdQ3zAt
-USDT (ETH Network)                                        0xa3095ecC94C75364cb9A0E1EE967Fbcb1C9B24B5
